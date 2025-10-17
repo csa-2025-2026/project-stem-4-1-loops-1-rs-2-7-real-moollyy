29f56fc94d258bb13c031630b0c40a1d76e4f82b
moollyy <136740176+moollyy@users.noreply.github.com>
2025-10-17 18:18:57 +0000

    1st done

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    int count = 3;
+
+    while(count <= 25)
+    {
+      System.out.println(count);
+      count += 2; 
+    }
+
+
   }
 }
